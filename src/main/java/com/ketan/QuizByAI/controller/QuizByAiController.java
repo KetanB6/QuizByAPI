4a10ba7d2699cb902aec77984dee78ed0a92ba79
Ketan Bidave <ketanpc77@gmail.com>
2026-01-28 11:04:15 +0530

    cross site origin update

--- src/main/java/com/ketan/QuizByAI/controller/QuizByAiController.java
+++ src/main/java/com/ketan/QuizByAI/controller/QuizByAiController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://myquizapp-psi.vercel/.app")
+@CrossOrigin(origins = "https://myquizapp-psi.vercel.app")
 public class QuizByAiController {
 
     @Autowired
