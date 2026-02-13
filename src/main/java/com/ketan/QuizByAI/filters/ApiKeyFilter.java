69eccce5a36f6947c7c5b573316848963c54092b
Ketan Bidave <ketanpc77@gmail.com>
2026-02-13 18:35:43 +0530

    Added web config for CORS

--- src/main/java/com/ketan/QuizByAI/filters/ApiKeyFilter.java
+++ src/main/java/com/ketan/QuizByAI/filters/ApiKeyFilter.java
@@ -18,10 +18,6 @@ public class ApiKeyFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-            filterChain.doFilter(request, response);
-            return;
-        }
 
         String apiKey = request.getHeader("X-API-KEY");
 
