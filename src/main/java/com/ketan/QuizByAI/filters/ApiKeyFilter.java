f03b4959513a8884464e67028d8125c8243ed7c3
Ketan Bidave <ketanpc77@gmail.com>
2026-02-13 15:53:03 +0530

    API key added for application

---
+++ src/main/java/com/ketan/QuizByAI/filters/ApiKeyFilter.java
@@ -0,0 +1,29 @@
+package com.ketan.QuizByAI.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class ApiKeyFilter extends OncePerRequestFilter {
+
+    @Value("${app.api.key}")
+    private String validApiKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String apiKey = request.getHeader("X-API-KEY");
+
+        if(apiKey.equals(validApiKey)) {
+            filterChain.doFilter(request, response);
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
+}
