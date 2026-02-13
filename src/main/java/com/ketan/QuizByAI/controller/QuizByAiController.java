83f25608a206bc73c827735d2c22b4da63e8c59d
Ketan Bidave <ketanpc77@gmail.com>
2026-02-13 18:39:46 +0530

    cors annotation removed

--- src/main/java/com/ketan/QuizByAI/controller/QuizByAiController.java
+++ src/main/java/com/ketan/QuizByAI/controller/QuizByAiController.java
@@ -11,8 +11,6 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-//@CrossOrigin(origins = "https://quizkrida.vercel.app")
-@CrossOrigin("*")
 @RequestMapping("/api/v1")
 public class QuizByAiController {
 
